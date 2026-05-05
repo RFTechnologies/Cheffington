7981f842b801ae3b5cc815d400cbd0297b57243b
Qasim Manzoor <qasimmanzoor282@gmail.com>
2026-05-05 19:19:16 +0500

    new

--- app/(main)/review/page.tsx
+++ app/(main)/review/page.tsx
@@ -171,7 +171,7 @@ export default function RestaurantSearch() {
               >
                 SEARCH
               </button>
-            </div>
+            </div> 
 
             {/* Dropdown */}
             {showDropdown && (
